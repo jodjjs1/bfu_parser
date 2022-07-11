9720abce2e0ca4430d63fab84aaaccb917ccf7f9
Daniil Zavyalov <jodjjs1@gmail.com>
2022-07-12 00:16:42 +0300

    make web page for cheking

---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, ValidationError
+from wtforms.validators import InputRequired
+
+def chek_snils_validator(form, field):
+        if len(field.data) > 14:
+            raise ValidationError('Некоректный снилс')
+
+class SnilsForm(FlaskForm):
+    snils = StringField('Снилс:', description='000-000-000 00' , validators=[InputRequired(), chek_snils_validator]) # написать валидатор
+    submit = SubmitField('Искать')
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
+from flask import request
+
+from forms import SnilsForm
+
+from get_mesto import GetMesto
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = ''
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
-    return 'Hello World'
+
+    form = SnilsForm()
+    if form.validate_on_submit():
+        snils = form.snils.data
+        res = redirect(url_for('your_place'))
+        res.set_cookie('snils', snils, max_age=60*60*24*7*4)
+        print(snils)
+        return res
+
+    return render_template('test_template.html', form=form)
+
+
+@app.route('/your_place/')
+def your_place():
+    snils = request.cookies.get('snils')
+    mesto_obj = GetMesto('')
+    mesto_obj.set_snils(snils)
+    mesta = mesto_obj.get_mesto()
+
+    return render_template('mesta.html', mesta=mesta)
 
 if __name__ == "__main__":
     app.run()
